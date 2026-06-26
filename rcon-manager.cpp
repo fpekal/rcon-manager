af7842020c00a138646266af8b21a90a89b7243a
Filip Pękala <272985@student.pwr.edu.pl>
2026-06-26 20:44:32 +0200

    Add TCP client

--- rcon-manager.cpp
+++ rcon-manager.cpp
@@ -1,18 +1,52 @@
+#include <arpa/inet.h>
 #include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #include "protocol.hpp"
 
 int main(int argc, char *argv[]) {
-  Packet p{123, PacketType::COMMAND, "say Hello, World!"};
-  auto raw_bytes = p.to_raw_bytes();
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-  for (auto b : raw_bytes) {
-    std::cout << static_cast<int>(b) << " ";
+  sockaddr_in servaddr;
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(25575);
+
+  inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+
+  if (connect(sockfd, reinterpret_cast<sockaddr *>(&servaddr),
+              sizeof(servaddr))) {
+    std::cerr << "Failed to connect" << std::endl;
+    return 1;
   }
-  std::cout << std::endl;
 
-  Packet p2 = Packet::from_raw_bytes(raw_bytes);
-  std::cout << p2.id << std::endl;
-  std::cout << p2.type << std::endl;
-  std::cout << p2.payload << std::endl;
+  auto send_packet = [](int sockfd, const Packet &packet) {
+    auto packet_raw = packet.to_raw_bytes();
+    write(sockfd, packet_raw.data(), packet_raw.size());
+  };
+
+  auto get_packet = [](int sockfd) {
+    std::byte buffer[1024];
+    size_t length = read(sockfd, buffer, sizeof(buffer));
+    return Packet::from_raw_bytes({buffer, buffer + length});
+  };
+
+  {
+    auto login = Packet::login("amogus");
+    send_packet(sockfd, login);
+
+    auto response = get_packet(sockfd);
+    std::cout << response.payload << std::endl;
+  }
+
+  while (true) {
+    std::string line;
+    std::getline(std::cin, line);
+    auto command = Packet::command(line);
+    send_packet(sockfd, command);
+
+    auto response = get_packet(sockfd);
+    std::cout << response.payload << std::endl;
+  }
 }
